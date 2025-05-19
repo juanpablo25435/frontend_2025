7e51fe30ce058237382bbd056f713fd9bac9df75
WilliamNOB <nanoitz98@gmail.com>
2025-05-19 00:45:33 -0500

    correción error operator_specialty

--- src/app/pages/operator-specialties/manage/manage.component.ts
+++ src/app/pages/operator-specialties/manage/manage.component.ts
@@ -14,7 +14,7 @@ import { SpecialtyService } from 'src/app/services/specialty.service';
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.scss']
 })
-export class ManageOperatorSpecialtyComponent implements OnInit {
+export class ManageComponent implements OnInit {
   mode: number; // 1->View, 2->Create, 3->Update
   operatorSpecialty: OperatorSpecialty;
   theFormGroup: FormGroup;
